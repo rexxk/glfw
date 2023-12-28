9289a3d86426ff891535ead3bf9cd27f9a8c4af4
rexxk <rexxk@bahnhof.se>
2023-12-28 19:04:18 +0100

    Added premake5.lua config file

---
+++ premake5.lua
@@ -0,0 +1,76 @@
+configurations { "Debug", "Release" }
+
+
+project "glfw"
+    kind "StaticLib"
+    language "C"
+
+    systemversion "latest"
+    staticruntime "on"
+
+    targetdir "%{wks.location}/bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"
+	objdir "%{wks.location}/bin-int/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"
+
+    files
+    {
+        "include/glfw/glfw3.h",
+        "include/glfw/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/platform.c",
+        "src/vulkan.c",
+        "src/window.c",
+    }
+
+    filter "system:linux"
+        pic "on"
+
+        files
+        {
+            "src/null_init.c",
+            "src/null_joystick.c",
+            "src/null_monitor.c",
+            "src/null_window.c",
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_module.c",
+            "src/posix_poll.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/glx_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+            "src/linux_joystick.c",
+        }
+
+        defines "_GLFW_X11"
+
+    filter "system:windows"
+
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+        }
+
+        defines "_GLFW_WIN32"
+
+    filter { "configurations:Debug" }
+        defines "DEBUG"
+        symbols "On"
+
+    filter { "configurations:Release" }
+        defines "NDEBUG"
+        optimize "On"
